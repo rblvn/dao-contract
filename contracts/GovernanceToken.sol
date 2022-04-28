8be8cdc045f484f7b70b8b83a665707eb80f4c3e
Ivan <sugudushka@gmail.com>
2022-04-28 18:58:56 +0300

    Update GovernanceToken.sol

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -1,11 +1,14 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@openzeppelin/contracts/tokens/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/tokens/ERC20/extentions/ERC20Votes.sol";
 
-contract GovernanceToken is ERC20 {
+contract GovernanceToken is ERC20Votes {
     uint256 public s_maxSupply = 1000000000000000000000000;
-    constructor() ERC20("GovernanceToken", "GT") {
+    constructor() 
+        ERC20("GovernanceToken", "GT")
+        ERC20Permit("GovernanceToken")
+    {
         _mint(msg.sender, s_maxSupply);
     }   
 }

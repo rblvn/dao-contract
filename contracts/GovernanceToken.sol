b99f898afb9f85621b231f53306b785aad162af7
Ivan <sugudushka@gmail.com>
2022-04-29 06:54:07 +0300

    added Governance contact

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@openzeppelin/contracts/tokens/ERC20/extentions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract GovernanceToken is ERC20Votes {
     uint256 public s_maxSupply = 1000000000000000000000000;
@@ -11,4 +11,19 @@ contract GovernanceToken is ERC20Votes {
     {
         _mint(msg.sender, s_maxSupply);
     }   
+  function _afterTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal override(ERC20Votes) {
+    super._afterTokenTransfer(from, to, amount);
+  }
+
+  function _mint(address to, uint256 amount) internal override(ERC20Votes) {
+    super._mint(to, amount);
+  }
+
+  function _burn(address account, uint256 amount) internal override(ERC20Votes) {
+    super._burn(account, amount);
+  }
 }
